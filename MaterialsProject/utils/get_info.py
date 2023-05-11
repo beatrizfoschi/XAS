9a2d185b011dc5116d13abefa795147a45ed1fc5
beatrizfoschi <108263485+beatrizfoschi@users.noreply.github.com>
2023-05-11 09:55:34 +0200

    Create get_info.py
    Get info from material id

---
+++ MaterialsProject/utils/get_info.py
@@ -0,0 +1,25 @@
+import pymatgen.core as mg
+from pymatgen.ext.matproj import _MPResterLegacy, MPRester as mpr
+
+def get_info(material_id):
+    """
+    Get the cif file from a given material using Materials Project Legacy database
+    ---------------------------------------------------------
+    material(str): material_id from Materials Project
+    """
+    with _MPResterLegacy("iZVT66G5iKswixmF") as mprl:
+        mpid = mprl.get_data(material_id)
+        cif = mpid[0]['cif']
+        icsd_ids = mpid[0]['icsd_ids']
+        space_group = mpid[0]['spacegroup']
+        formula = mpid[0]['pretty_formula']
+        info = {f'{material_id}':{'formula': f'{formula}', 'icsd_ids':f'{icsd_ids}', 'cif': f'{cif}', 'spacegroup': f'{space_group}'}}
+        return info
+        
+
+
+def get_cif(material_id): 
+    with _MPResterLegacy("iZVT66G5iKswixmF") as mprl:
+        mpid = mprl.get_data(material_id)
+        cif = mpid[0]['cif']
+        return cif
