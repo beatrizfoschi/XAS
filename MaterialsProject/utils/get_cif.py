2eceda0548e65179066dc48c0108b203918231fa
Beatriz Foschiani <beatrizgfoschiani@gmail.com>
2023-08-14 10:44:57 +0200

    delete get_cif.py

--- MaterialsProject/utils/get_cif.py
+++
@@ -1,9 +0,0 @@
-import pymatgen.core as mg
-from pymatgen.ext.matproj import _MPResterLegacy, MPRester as mpr
-
-mpi_key = "******"
-def get_cif(material_id): 
-    with _MPResterLegacy("mpi_key") as mprl:
-        mpid = mprl.get_data(material_id)
-        cif = mpid[0]['cif']
-        return cif
